36a912cfaa4cef8e64c1b38b89aa4d653c1f3ffa
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-03-27 22:37:52 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,11 +1,11 @@
 def square_array(array)
   # your code here
 counter = 0
-new_array=[]
+new_numbers=[]
 while array[counter] do
   puts array[counter]
-  new_array[counter] = array[counter]**2
-  puts new_array[counter]
+  new_numbers[counter] = array[counter]**2
+  puts new_numbers[counter]
   counter +=1
 end
 end
